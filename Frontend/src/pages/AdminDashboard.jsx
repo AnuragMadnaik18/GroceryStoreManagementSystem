832fff86a6fe4133b986809bb54135ec5788ab33
AnuragMadnaik18 <anuragmadnaik9381@gmail.com>
2025-08-06 20:02:49 +0530

    Deleted few files due its relocation in foler and not required

--- Frontend/src/pages/AdminDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AdminDashboard = () => {
-  return <h1 style={{ textAlign: 'center', marginTop: '50px' }}>Welcome Admin!</h1>;
-};
-
-export default AdminDashboard;
--- Frontend/src/pages/UserRegistration.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const UserRegistration = () => {
-  return <h1 style={{ textAlign: 'center', marginTop: '50px' }}>Welcome User!</h1>;
-};
-
-export default UserRegistration;
